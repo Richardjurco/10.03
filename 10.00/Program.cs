f72119c6822843c2a5403a118ce5a66d0e023c60
Richardjurco <113421214+Richardjurco@users.noreply.github.com>
2023-01-11 18:04:45 +0100

    Update Program.cs

--- 10.00/Program.cs
+++ 10.00/Program.cs
@@ -1,23 +1,26 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _10._00
+namespace Open_Lab_10._03
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Book LOTR = new Book();
-            LOTR.Title("Lord of the Rings");
-            LOTR.Pages(576);
-            LOTR.Category("Fantasy");
-            LOTR.Author("J.R.R.Tolkien");
-            LOTR.ReleaseDate(1954);
-            LOTR.vypis();
+            LOTR.Category = "Roman";
+            LOTR.Title = "Lord Of The Rings";
+            LOTR.Releasedate = 1989;
+            LOTR.Author = "Michaelo";
+            LOTR.Pages = 100;
+            Console.WriteLine(LOTR.ToString());
 
         }
     }
+}
 }
